440c6ec8b3e081e7d99f0800970c08f0a1f55b5d
Matthew Cantele <m.cantele@unimelb.edu.au>
2025-11-11 11:35:24 +1300

    sets now allow flexible case

--- R/chk_sets_consistency.R
+++ R/chk_sets_consistency.R
@@ -1,20 +1,28 @@
+#' @importFrom purrr map2_lgl
 #' @keywords internal
 #' @noRd
 .check_set_consistency <- function(bin_sets,
                                    tab_sets,
                                    call) {
-  if (!all(names(tab_sets) %in% bin_sets$setname)) {
-    x_sets <- setdiff(names(tab_sets), bin_sets$setname)
+
+  tab_set_names <- tolower(names(tab_sets))
+  if (!all(tab_set_names %in% bin_sets$setname)) {
+    x_sets <- toupper(setdiff(tab_set_names, bin_sets$setname))
     .cli_action(compose_err$missing_sets,
-                action = "abort",
-                call = call)
+      action = "abort",
+      call = call
+    )
   }
-  bin_sets <- subset(bin_sets,
-                     setname %in% names(tab_sets))
-  r_idx <- match(bin_sets$setname, names(tab_sets))
-  if (!all(purrr::map2_lgl(tab_sets[r_idx],
-              bin_sets$ele,
-              all.equal))) {
+  bin_sets <- subset(
+    bin_sets,
+    setname %in% tab_set_names
+  )
+  r_idx <- match(bin_sets$setname, tab_set_names)
+  if (!all(purrr::map2_lgl(
+    tab_sets[r_idx],
+    bin_sets$ele,
+    all.equal
+  ))) {
     .cli_action(compose_err$set_mismatch,
       action = "abort",
       call = call
