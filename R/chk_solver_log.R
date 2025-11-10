ba884934f1f32aa3c1178e232df92272bf3ec0e1
Matthew Cantele <m.cantele@unimelb.edu.au>
2025-11-11 11:47:45 +1300

    singularity check before general error check

--- R/chk_solver_log.R
+++ R/chk_solver_log.R
@@ -7,17 +7,17 @@
   
   paths$diag_out
   model_log <- readLines(paths$diag_out)
-  if (any(grepl("Error", model_log, ignore.case = TRUE))) {
-    .cli_action(solve_err$solution_err,
-                action = "abort",
-                call = call)
-  } 
   if (any(grepl(pattern = "singular", model_log, ignore.case = TRUE))) {
     .cli_action(solve_err$solution_sing,
                 action = "abort",
                 call = call)
   }
-  
+  if (any(grepl("error", model_log, ignore.case = TRUE))) {
+    .cli_action(solve_err$solution_err,
+                action = "abort",
+                call = call)
+  } 
+
   writeLines(solve_cmd, file.path(paths$run, "model_exec.txt"))
   .inform_diagnostics(
     elapsed_time = elapsed_time,
