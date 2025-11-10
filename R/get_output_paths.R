7009f1c29ace0d39679a8bc16ac5a3a5c2c97c2b
Matthew Cantele <m.cantele@unimelb.edu.au>
2025-11-11 12:14:13 +1300

    no change

--- R/get_output_paths.R
+++ R/get_output_paths.R
@@ -4,6 +4,7 @@
                               which,
                               select = NULL,
                               call) {
+
   model_dir <- dirname(normalizePath(cmf_path))
   if ("tab_path" %in% names(attributes(cmf_path))) {
     tab_path <- attr(cmf_path, "tab_path")
